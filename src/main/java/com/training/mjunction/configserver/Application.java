63bd53197ba35d0242d63f9536dd01b07010a4b0
talus005 <expogrow.org@gmail.com>
2019-02-12 18:56:08 +0530

    Initial Commit

---
+++ src/main/java/com/training/mjunction/configserver/Application.java
@@ -0,0 +1,22 @@
+package com.training.mjunction.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+	public static void main(final String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
