e9313554ce128e6ae86ddeb7a989100826c3373a
talus005 <sanjib.x.talukdar.-nd@disney.com>
2019-05-26 14:39:55 +0530

    added bean

--- src/main/java/com/training/mjunction/configserver/Application.java
+++ src/main/java/com/training/mjunction/configserver/Application.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
 
 @SpringBootApplication
 @EnableConfigServer
@@ -20,5 +23,11 @@ public class Application extends SpringBootServletInitializer {
 	public static void main(final String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
+	
+	@Bean
+    public Sampler defaultSampler() {
+        return Sampler.ALWAYS_SAMPLE;
+    }
 
 }
